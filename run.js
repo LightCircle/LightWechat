fec985866b866ae912d0f19b891960798e684ca8
LI LIN <r2space@gmail.com>
2016-01-21 15:48:16 +0800

    为了使用代理，修改公众号api的请求方法

--- run.js
+++ run.js
@@ -13,14 +13,14 @@ var log       = light.framework.log
   , cache     = light.framework.cache
   , context   = light.framework.context
   , rider     = light.model.rider
-  , mp        = require("./lib/mp")
   , eapi      = require("./lib/enterprise_api")
+  , mapi      = require("./lib/mp_api");
   ;
 
 process.env.LIGHTDB_USER = "r2space";
 process.env.LIGHTDB_PASS = "ranbow";
 process.env.LIGHTDB_PORT = "57017";
-process.env.APPNAME = "401a86a8af87";
+process.env.APPNAME = "56648deee144";
 process.env.DEV = true;
 
 cache.manager.init(process.env.APPNAME, function (err) {
@@ -32,8 +32,19 @@ cache.manager.init(process.env.APPNAME, function (err) {
   // 初始化rider
   rider.init();
 
-  eapi.sendText({touser: "r2space", text: {content: "测试 企业号 发送消息"}}, function (err, result) {
+  // 测试公众号的主动调用api
+  new mapi().getJsConfig({
+    debug: true,
+    jsApiList: ["chooseImage", "uploadImage"],
+    url: "http://56648deee144.app.alphabets.cn"
+  }, function (err, result) {
+
     console.log(err, result);
   });
+
+  // 测试企业号的主动调用api
+  //eapi.sendText({touser: "r2space", text: {content: "测试 企业号 发送消息"}}, function (err, result) {
+  //  console.log(err, result);
+  //});
 
 });
