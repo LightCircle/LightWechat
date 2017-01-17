e2a7c0db65291e125d450b8e725a5d21ba5003ff
LI LIN <r2space@gmail.com>
2017-01-17 21:00:57 +0800

    修改路径，api包版本升级

--- index.js
+++ index.js
@@ -7,6 +7,6 @@
 
 exports.mp = require("./lib/mp/mp");
 exports.mpAPI = require("./lib/mp/mp_api");
-exports.enterprise = require("./lib/enterprise/enterprise");
-exports.enterpriseAPI = require("./lib/enterprise/enterprise_api");
-exports.xcx = require("./lib/xcx/api");
+exports.ep = require("./lib/ep/enterprise");
+exports.epAPI = require("./lib/ep/enterprise_api");
+exports.xcxAPI = require("./lib/xcx/api");
